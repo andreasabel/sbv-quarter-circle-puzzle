5d047f38cd227252ce303f2fdaca657ae7221b75
Andreas Abel <andreas.abel@ifi.lmu.de>
2024-06-04 12:02:32 +0200

    Refactor: represent board just as a list (1-dim instead of 2-dim)

--- src/Main.hs
+++ src/Main.hs
@@ -6,6 +6,9 @@
 
 -- import Prelude hiding (pi)
 
+-- import Control.Monad ((<=<))
+import Data.List.Extra ((!?))
+
 import Data.Map (Map)
 import Data.Map qualified as Map
 
@@ -15,7 +18,7 @@ main = putStrLn "Hello puzzle two!"
 -- * Puzzle
 ------------------------------------------------------------------------
 
-type Puzzle = [[Val]]
+type Puzzle = [Val]
 
 data Val = Val { quaters :: Integer, piQuaters :: Integer }
   deriving (Eq, Ord)
@@ -44,7 +47,7 @@ instance Num Val where
 π = Val 0 scale
 
 puzzle1 :: Puzzle
-puzzle1 =
+puzzle1 = concat
   [ [ 0, 1, 6, 0]
   , [ 6, 0, π, 2]
   , [ 1, π, 0, 6]
@@ -54,7 +57,7 @@ puzzle1 =
 -- * Solution
 ------------------------------------------------------------------------
 
-type Solution = [[Line]]
+type Solution = [Line]
 
 -- | Possible square fillings.
 data Line
@@ -66,7 +69,7 @@ data Line
   deriving (Eq, Ord, Show)
 
 solution1 :: Solution
-solution1 =
+solution1 = concat
   [ [ NE, SE, NE, OO ]
   , [ SW, NW, NE, SW ]
   , [ SE, SW, SE, OO ]
@@ -76,7 +79,7 @@ solution1 =
 -- | Cluster IDs.
 type ID = Integer
 
-type Partition = [[Partitioning]]
+type Partition = [Partitioning]
 
 -- | Divide each square via the diagonals into 4 areas, each of which
 -- gets an ID of its cluser.
@@ -84,89 +87,80 @@ data Partitioning = P { north, west, south, east :: ID }
   deriving (Show)
 
 partition1 :: Partition
-partition1 =
+partition1 = concat
   [ [ P 1 0 0 1, P 1 1 0 0, P 2 0 0 2, P 2 2 2 2 ]
   , [ P 0 3 3 0, P 0 0 4 4, P 0 4 4 0, P 2 0 0 2 ]
   , [ P 3 3 6 6, P 4 6 6 4, P 4 4 0 0, P 0 0 0 0 ]
   , [ P 6 5 5 6, P 6 6 7 7, P 0 7 7 0, P 0 0 8 8 ]
   ]
 
-data Dim = Dim { height, width :: Int }
+type Height = Int
+type Width  = Int
+
+data Dim = Dim { height :: Height, width :: Width  }
   deriving (Show)
 
 dim1 :: Dim
 dim1 = Dim 4 4
 
-data Coord = Coord { vert, horiz :: Int }
-  deriving Eq
+type Size = Int
 
--- | Coordinate inside board.
-validCoord :: Dim -> Coord -> Bool
-validCoord (Dim h w) (Coord ve ho) = and [ ve >= 0, ve < h, ho >= 0, ho < w ]
+size :: Dim -> Size
+size (Dim h w) = h * w
 
-goNorth :: Coord -> Maybe Coord
-goNorth (Coord ve ho) = if ve > 0 then Just $ Coord (ve - 1) ho else Nothing
+type Coord = Int
 
-goWest :: Coord -> Maybe Coord
-goWest (Coord ve ho) = if ho > 0 then Just $ Coord ve (ho - 1) else Nothing
+filterMaybe :: (a -> Bool) -> a -> Maybe a
+filterMaybe p a = if p a then Just a else Nothing
+
+goNorth :: Dim -> Coord -> Maybe Coord
+goNorth (Dim _ w)     x = filterMaybe (>= 0) $ x - w
+
+goWest :: Dim -> Coord -> Maybe Coord
+goWest (Dim _ w)      x = filterMaybe (const $ x `mod` w > 0) $ x - 1   -- prevent wrap
 
 goSouth :: Dim -> Coord -> Maybe Coord
-goSouth (Dim h _) (Coord ve ho) = if ve < h - 1 then Just $ Coord (ve + 1) ho else Nothing
+goSouth dim@(Dim _ w) x = filterMaybe (< size dim) $ x + w
 
 goEast :: Dim -> Coord -> Maybe Coord
-goEast (Dim _ w) (Coord ve ho) = if ho < w - 1 then Just $ Coord ve (ho + 1) else Nothing
+goEast (Dim _ w)      x = filterMaybe ((0 /=) . (`mod` w)) $ x + 1      -- prevent wrap
 
--- validPartitioning :: Dim -> Solution -> Partition -> Bool
--- validPartitioning (Dim height width)
-
--- sameID :: Dim -> Partition -> ID -> Maybe Coord -> Bool
--- sameID dim part i = \case
---   Nothing -> True
---   Just coord -> i == part ! coord
-
-(!) :: [[a]] -> Coord -> a
-board ! Coord ve ho = board !! ve !! ho
-
-dzipWith2 :: (a -> b -> c) -> [[a]] -> [[b]] -> [[c]]
-dzipWith2 f = zipWith $ zipWith f
-
-dzipWith3 :: (a -> b -> c -> d) -> [[a]] -> [[b]] -> [[c]] -> [[d]]
-dzipWith3 f = zipWith3 $ zipWith3 f
-
-coordinates :: Dim -> [[Coord]]
-coordinates (Dim height width) = map (\ ve -> map (Coord ve) [0..width-1]) [0..height-1]
+coordinates :: Dim -> [Coord]
+coordinates dim = [0 .. size dim - 1]
 
 validSquare :: Dim -> Partition -> Coord -> Partitioning -> Line -> Bool
 validSquare dim part coord (P n w s e) = \case
   -- quarter square N to W: S and E have same ID
   NW -> and [ n == w, s == e
-            , maybe True ((n ==) . south . (part !)) $ goNorth     coord
-            , maybe True ((w ==) . east  . (part !)) $ goWest      coord
+            , maybe True ((n ==) . south) $ (part !?) =<< goNorth dim coord
+            , maybe True ((w ==) . east ) $ (part !?) =<< goWest  dim coord
             ]
   SW -> and [ s == w, n == e
-            , maybe True ((s ==) . north . (part !)) $ goSouth dim coord
-            , maybe True ((w ==) . east  . (part !)) $ goWest      coord
+            , maybe True ((s ==) . north) $ (part !?) =<< goSouth dim coord
+            , maybe True ((w ==) . east ) $ (part !?) =<< goWest  dim coord
             ]
   NE -> and [ n == e, s == w
-            , maybe True ((n ==) . south . (part !)) $ goNorth     coord
-            , maybe True ((e ==) . west  . (part !)) $ goEast  dim coord
+            , maybe True ((n ==) . south) $ (part !?) =<< goNorth dim coord
+            , maybe True ((e ==) . west ) $ (part !?) =<< goEast  dim coord
             ]
   SE -> and [ s == e, n == w
-            , maybe True ((s ==) . north . (part !)) $ goSouth dim coord
-            , maybe True ((e ==) . west  . (part !)) $ goEast  dim coord
+            , maybe True ((s ==) . north) $ (part !?) =<< goSouth dim coord
+            , maybe True ((e ==) . west ) $ (part !?) =<< goEast  dim coord
             ]
   OO -> and [ n == w, w == s, s == e
-            , maybe True ((n ==) . south . (part !)) $ goNorth     coord
-            , maybe True ((w ==) . east  . (part !)) $ goWest      coord
-            , maybe True ((s ==) . north . (part !)) $ goSouth dim coord
-            , maybe True ((e ==) . west  . (part !)) $ goEast  dim coord
+            , maybe True ((n ==) . south) $ (part !?) =<< goNorth dim coord
+            , maybe True ((w ==) . east ) $ (part !?) =<< goWest  dim coord
+            , maybe True ((s ==) . north) $ (part !?) =<< goSouth dim coord
+            , maybe True ((e ==) . west ) $ (part !?) =<< goEast  dim coord
             ]
 
 validPartition :: Dim -> Partition -> Solution -> Bool
-validPartition dim part sol = all and $
-  dzipWith3 (validSquare dim part) (coordinates dim) part sol
+validPartition dim part sol = and $
+  zipWith3 (validSquare dim part) (coordinates dim) part sol
 
 validPartition1 = validPartition dim1 partition1 solution1
+
+vp n = validPartition dim1 (take n partition1) (take n solution1)
 
 -- | Valuation of a partition (accumulated values).
 type Valuation = Map ID Val
@@ -192,7 +186,7 @@ valueSquare (P n w s e) = \case
   OO -> Map.singleton n 1
 
 valuePartition :: Partition -> Solution -> Valuation
-valuePartition part sol = Map.unionsWith (+) $ concat $ dzipWith2 valueSquare part sol
+valuePartition part sol = Map.unionsWith (+) $ zipWith valueSquare part sol
 
 valuation1 = valuePartition partition1 solution1
 
@@ -212,7 +206,7 @@ validValue m 0 p l = True
 validValue m v p l = v == m Map.! idOf p l
 
 validValues :: Valuation -> Puzzle -> Partition -> Solution -> Bool
-validValues m puzzle part sol = all and $ dzipWith3 (validValue m) puzzle part sol
+validValues m puzzle part sol = and $ zipWith3 (validValue m) puzzle part sol
 
 vv1 = validValues valuation1 puzzle1 partition1 solution1
 
