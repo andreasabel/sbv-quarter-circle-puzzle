51308ee00a9b7b2d6017b7194232bfe058a2f939
Andreas Abel <andreas.abel@ifi.lmu.de>
2024-06-06 15:11:43 +0200

    Final MWE: just + on (Integer, Integer)

--- MWE.hs
+++ MWE.hs
@@ -4,17 +4,13 @@ type Val = (Integer, Integer)
 
 instance Num Val where
   fromInteger i       = (i, 0)
-  negate (x, y)       = (negate x, negate y)
   (x1, y1) + (x2, y2) = (x1 + x2, y1 + y2)
   (*)    = undefined
   abs    = undefined
+  negate = undefined
   signum = undefined
 
-type SVal = SBV Val
-
-valVec :: Int -> Symbolic [SVal]
-valVec n = mapM (\ k -> symbolic ("X" ++ show k)) [0..n-1]
-
 main = satWith z3{ verbose = True } $ do
-  v <- valVec 3
-  pure $ sum v .== 1
+  x :: SBV Val <- symbolic "x"
+  y :: SBV Val <- symbolic "y"
+  pure $ x + y .== 1
